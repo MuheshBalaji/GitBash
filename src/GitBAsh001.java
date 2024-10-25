71bde21737f592c96d9230d95cb26c5ca37f23a6
MuheshBalaji <muheshbalaji20@gmail.com>
2024-10-25 18:04:43 +0530

    commit123

---
+++ src/GitBAsh001.java
@@ -0,0 +1,14 @@
+
+public class GitBAsh001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2 or 3 days once Build will be delivered");
+		
+		System.out.println("3 or 4 weeks one sprint will be completed");
+		
+		System.out.println("Depend on Duration of the project number of Release ..");
+
+	}
+
+}
